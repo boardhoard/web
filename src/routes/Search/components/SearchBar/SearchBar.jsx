8e7afb5cc61560071eb614977f0ae839ee3fc403
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2019-02-09 20:07:51 -0500

    Add search bar component

---
+++ src/routes/Search/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import DirectionsIcon from '@material-ui/icons/Directions';
+
+const styles = {
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+    width: '97%',
+    margin: '0.3rem',
+  },
+  input: {
+    marginLeft: '0.5rem',
+    flex: 1,
+  },
+  iconButton: {
+    padding: '0.5rem',
+  }
+};
+
+function SearchBar(props) {
+  const { classes } = props;
+
+  return (
+    <Paper className={classes.root} elevation={3}>
+      {/* <IconButton className={classes.iconButton} aria-label="Menu">
+        <MenuIcon />
+      </IconButton> */}
+      <InputBase className={classes.input} placeholder="Search Boardgames" />
+      <IconButton className={classes.iconButton} aria-label="Search">
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  );
+}
+
+SearchBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SearchBar);
