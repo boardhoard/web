a1dcf3596f5764d4e835cda8f67d11a1783d18d1
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2019-02-09 20:07:57 -0500

    Export routes

---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+export {
+    default as Auth,
+} from './Auth';
+
+export {
+    default as Home,
+} from './Home';
+
+export {
+    default as Library,
+} from './Library';
+
+export {
+    default as Search,
+} from './Search';
