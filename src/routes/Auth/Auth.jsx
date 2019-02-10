3458246a12de21e1d612b2158b9c277d71afea92
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2019-02-09 20:06:36 -0500

    Add auth component that uses Login

---
+++ src/routes/Auth/Auth.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import './Auth.css';
+import {Login} from '../../components';
+import { Redirect } from 'react-router';
+
+
+class App extends React.Component {
+  state = {
+  };
+
+  render() {
+    const {isAuth} = this.state;
+    const GoTo = isAuth ? <Redirect to='/onboarding' /> : null
+
+    const handleSuccessfulLoginRedirect = () => {
+      this.setState({isAuth: true})
+      console.log('Successful Login')
+    }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className='title'>Boardhoard</h1>
+          <Login onSuccess={handleSuccessfulLoginRedirect}/>
+          <svg className='wave' id="svg" xmlns="http://www.w3.org/2000/svg" viewBox="-300 0 950 270" >
+            <path d="M-314,267 C105,364 400,100 812,279" fill="none" stroke="white" stroke-width="120" stroke-linecap="round"/>
+          </svg>
+        </header>
+        {GoTo}
+      </div>
+    );
+  }
+}
+
+export default App;
