84bbbc32e1e29d02ee32e3ff07653d1aacee14f9
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2019-02-12 14:26:14 -0500

    scaffolding to do top method

---
+++ src/utilities/BoardGameGeek.js
@@ -0,0 +1,11 @@
+import convert from 'xml-js';
+
+const top = ({/*some params like number of objects and page?*/}) => {
+    //Do a fetch to the top bgg API 
+    // Use 'convert' methods here: https://www.npmjs.com/package/xml-js
+    // return the json object.
+}
+
+
+
+export {top}
