48c3f094e7329481155b6fa12a61a278088855b3
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2019-02-17 14:16:24 -0500

    convert xml

--- src/utilities/BoardGameGeek.js
+++ src/utilities/BoardGameGeek.js
@@ -1,6 +1,12 @@
 import convert from 'xml-js';
 
-const top = ({/*some params like number of objects and page?*/}) => {
+const top = async () => {
+    const response = await fetch('https://www.boardgamegeek.com/xmlapi2/hot', {
+        method: 'GET',
+    })
+    const json = convert.xml2js(await response.text(), {compact: true, spaces: 4});
+    console.log(json.items.item)
+
     //Do a fetch to the top bgg API 
     // Use 'convert' methods here: https://www.npmjs.com/package/xml-js
     // return the json object.
